462165e7fd657f83def81542d279bc595cdb48ae
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-05-07 20:01:28 +0700

    create project

---
+++ src/main/java/com/example/exe202backend/Exe202BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.exe202backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exe202BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exe202BackendApplication.class, args);
+    }
+
+}
