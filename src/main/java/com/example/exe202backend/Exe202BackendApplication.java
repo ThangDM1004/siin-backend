b2ae9dd6002c245cc63ceec48a6486f64b420348
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-05-29 13:41:50 +0700

    add payOS and update payment when payment success

--- src/main/java/com/example/exe202backend/Exe202BackendApplication.java
+++ src/main/java/com/example/exe202backend/Exe202BackendApplication.java
@@ -1,12 +1,15 @@
 package com.example.exe202backend;
 
+import com.lib.payos.PayOS;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -26,7 +29,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         }
 )
 public class Exe202BackendApplication {
+    @Value("${PAYOS_CLIENT_ID}")
+    private String clientId;
 
+    @Value("${PAYOS_API_KEY}")
+    private String apiKey;
+
+    @Value("${PAYOS_CHECKSUM_KEY}")
+    private String checksumKey;
+    @Bean
+    public PayOS payOS() {
+        return new PayOS(clientId, apiKey, checksumKey);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Exe202BackendApplication.class, args);
     }
