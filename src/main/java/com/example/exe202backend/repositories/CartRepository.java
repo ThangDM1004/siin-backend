cde8a4fc52cc6ab18612c83915a95136b3bcf5ad
hieu15402 <hieunm15402@gmail.com>
2024-05-31 11:32:29 +0700

    get-cart-items-by-user-id

--- src/main/java/com/example/exe202backend/repositories/CartRepository.java
+++ src/main/java/com/example/exe202backend/repositories/CartRepository.java
@@ -4,10 +4,7 @@ import com.example.exe202backend.models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByUserId(Long userId);
 }
