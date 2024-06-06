841e1f4b6300fe666d7ea52d554f279286e5fa71
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-06-06 23:41:23 +0700

    get product by categoryId

--- src/main/java/com/example/exe202backend/controllers/ProductController.java
+++ src/main/java/com/example/exe202backend/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.exe202backend.controllers;
 
 import com.example.exe202backend.dto.PageList;
 import com.example.exe202backend.dto.ProductDTO;
+import com.example.exe202backend.models.Product;
 import com.example.exe202backend.response.ResponseObject;
 import com.example.exe202backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,20 +24,39 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/get-all/{currentPage}")
-    public ResponseEntity<ResponseObject> getAll(@PathVariable int currentPage
-            , @RequestParam(defaultValue = "5") int pageSize
-            , @RequestParam(defaultValue = "name") String field) {
-        if(currentPage < 1 || pageSize < 1 || currentPage > pageSize){
-            return ResponseEntity.ok(new ResponseObject("get success", productService.get()));
+    public ResponseEntity<ResponseObject> getAll(
+            @RequestParam(defaultValue = "0") int currentPage,
+            @RequestParam(defaultValue = "5") int pageSize,
+            @RequestParam(defaultValue = "name") String field,
+            @RequestParam(required = false, defaultValue = "0") long categoryId) {
+        if (categoryId == 0) {
+            if (currentPage < 1 || pageSize < 1 || currentPage > pageSize) {
+                return ResponseEntity.ok(new ResponseObject("get success", productService.get()));
+            }
+
+            Page<ProductDTO> accessories = productService.getAll(currentPage, pageSize, field);
+            var pageList = PageList.<ProductDTO>builder()
+                    .totalPage(accessories.getTotalPages())
+                    .currentPage(currentPage)
+                    .listResult(accessories.getContent())
+                    .build();
+            return ResponseEntity.ok(new ResponseObject("get success", pageList));
+        }else{
+            if (currentPage < 1 || pageSize < 1 || currentPage > pageSize) {
+                return ResponseEntity.ok(new ResponseObject("get success", productService.getByCategory(categoryId)));
+            }
+
+            Page<ProductDTO> accessories = productService.searchByCategory(currentPage, pageSize, field,categoryId);
+            var pageList = PageList.<ProductDTO>builder()
+                    .totalPage(accessories.getTotalPages())
+                    .currentPage(currentPage)
+                    .listResult(accessories.getContent())
+                    .build();
+            return ResponseEntity.ok(new ResponseObject("get success", pageList));
         }
-        Page<ProductDTO> accessories = productService.getAll(currentPage, pageSize, field);
-        var pageList = PageList.<ProductDTO>builder()
-                .totalPage(accessories.getTotalPages())
-                .currentPage(currentPage)
-                .listResult(accessories.getContent())
-                .build();
-        return ResponseEntity.ok(new ResponseObject("get success", pageList));
+
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<ResponseObject> getById(@PathVariable long id) {
         return productService.getById(id);
@@ -46,10 +66,12 @@ public class ProductController {
     public ResponseEntity<ResponseObject> create(@RequestBody ProductDTO productDTO) {
         return productService.create(productDTO);
     }
+
     @PutMapping("/{id}")
-    public ResponseEntity<ResponseObject> update(@PathVariable long id,@RequestBody ProductDTO productDTO) {
-        return productService.update(id,productDTO);
+    public ResponseEntity<ResponseObject> update(@PathVariable long id, @RequestBody ProductDTO productDTO) {
+        return productService.update(id, productDTO);
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<ResponseObject> delete(@PathVariable long id) {
         return productService.delete(id);
@@ -57,16 +79,18 @@ public class ProductController {
 
     @PostMapping("/upload-image/{productId}")
     public ResponseEntity<ResponseObject> uploadImage(@RequestParam("file") MultipartFile multipartFile,
-                                                          @PathVariable Long productId ) {
+                                                      @PathVariable Long productId) {
         return productService.createCoverImage(multipartFile, productId);
     }
+
     @PutMapping("/update-image/{productId}")
     public ResponseEntity<ResponseObject> updateImage(@RequestParam("file") MultipartFile multipartFile,
-                                                          @PathVariable Long productId ) throws IOException, URISyntaxException {
+                                                      @PathVariable Long productId) throws IOException, URISyntaxException {
         return productService.updateCoverImage(multipartFile, productId);
     }
+
     @DeleteMapping("/delete-image/{productId}")
-    public ResponseEntity<ResponseObject> uploadImage(@PathVariable Long productId ) throws IOException, URISyntaxException {
+    public ResponseEntity<ResponseObject> uploadImage(@PathVariable Long productId) throws IOException, URISyntaxException {
         return productService.deleteCoverImage(productId);
     }
 }
